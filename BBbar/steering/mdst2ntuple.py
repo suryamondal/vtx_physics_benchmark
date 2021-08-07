02c68104e664bc8fdb025ebc3e9bfa5fca00aebd
Ludovico Massaccesi <l.massaccesi@studenti.unipi.it>
2021-08-07 18:58:51 +0900

    Bugfix (wrong ranking order).

--- BBbar/steering/mdst2ntuple.py
+++ BBbar/steering/mdst2ntuple.py
@@ -220,8 +220,8 @@ ma.matchMCTruth(list_name='B0:good', path=main)
 
 # Best-candidate selection (does not cut, only adds the rank variable)
 ma.rankByHighest("B0:good", "M", allowMultiRank=True, path=main)
-ma.rankByHighest("B0:good", "daughter(0,abs(dM))", allowMultiRank=True, outputVariable="Dst_dM_rank", path=main)
-ma.rankByHighest("B0:good", "daughter(0,daughter(0,abs(dM)))", allowMultiRank=True, outputVariable="D0_dM_rank", path=main)
+ma.rankByLowest("B0:good", "daughter(0,abs(dM))", allowMultiRank=True, outputVariable="Dst_dM_rank", path=main)
+ma.rankByLowest("B0:good", "daughter(0,daughter(0,abs(dM)))", allowMultiRank=True, outputVariable="D0_dM_rank", path=main)
 
 # Variables of the event
 eventWiseVariables = ['IPX', 'IPY', 'IPZ', 'genIPX', 'genIPY', 'genIPZ',
@@ -295,12 +295,12 @@ for v in ['px', 'py', 'pz', 'p']:
 # Angle between pi and K
 vm.addAlias('Kpi_MCAngle', 'daughter(0,daughter(0,mcDaughterAngle(0,1)))')
 vm.addAlias("B0_M_rank", "extraInfo(M_rank)")
-vm.addAlias("Dst_M_rank", "extraInfo(Dst_dM_rank)")
-vm.addAlias("D0_M_rank", "extraInfo(D0_dM_rank)")
+vm.addAlias("Dst_dM_rank", "extraInfo(Dst_dM_rank)")
+vm.addAlias("D0_dM_rank", "extraInfo(D0_dM_rank)")
 
 # Final output variables
-varsKpi += cms_variables + eventWiseVariables + ['Kpi_MCAngle', "B0_M_rank", "Dst_M_rank", "D0_M_rank"]
-varsK3pi += cms_variables + eventWiseVariables + ["B0_M_rank", "Dst_M_rank", "D0_M_rank"]
+varsKpi += cms_variables + eventWiseVariables + ['Kpi_MCAngle', "B0_M_rank", "Dst_dM_rank", "D0_dM_rank"]
+varsK3pi += cms_variables + eventWiseVariables + ["B0_M_rank", "Dst_dM_rank", "D0_dM_rank"]
 varsKpi.sort()  # I want to be able to find what I need quickly
 varsK3pi.sort()
 if args.printVars:
